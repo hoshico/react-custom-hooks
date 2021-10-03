c79e93cb92544a3dbd6dc784eea4639d6dd65609
hoshico <77495217+hoshico@users.noreply.github.com>
2021-10-04 08:26:43 +0900

    some fix

--- src/types/api/user.ts
+++ src/types/api/user.ts
@@ -4,21 +4,21 @@ export type User = {
     username: string;
     email: string;
     address: {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-    geo: {
-    lat: string;
-    lng: string
-    }
+      street: string;
+      suite: string;
+      city: string;
+      zipcode: string;
+      geo: {
+        lat: string;
+        lng: string
+      }
     };
     phone: string;
     website: string;
     company: {
-    name: string;
-    catchPhrase: string;
-    bs: string;
-    }
-    
+      name: string;
+      catchPhrase: string;
+      bs: string;
+    };
+}
     
