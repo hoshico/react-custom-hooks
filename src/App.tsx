ae46a2a9961130014d02185f4e42b22bcbc5f0d3
hoshico <77495217+hoshico@users.noreply.github.com>
2021-10-06 07:48:00 +0900

    add error / loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,52 @@
 import "./styles.css";
-import { UserCard } from "./componets/UserCard"
+import { UserCard } from "./componets/UserCard";
 import axios from "axios";
 import { User } from "./types/api/user";
 import { userProfile } from "./types/userProfile";
 import { useState } from "react";
 
-const user = {
-  id: "1",
-  name: "じゃけぇ",
-  email: "1213@aaa.com",
-  address: "ADDRESS"
-}
-
 export default function App() {
-const [useeProfiles, setUserProfiles] = useState<Array<userProfile>>([]);
-  
-const onClickFetchUser = () => {
+  const [userProfiles, setUserProfiles] = useState<Array<userProfile>>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const onClickFetchUser = () => {
+    setLoading(true);
+    setError(false);
+
     axios
       .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
       .then((res) => {
-        const data = res.data.map((user) =>({
+        const data = res.data.map((user) => ({
           id: user.id,
           name: `${user.name}(${user.username})`,
           email: user.email,
           address: `${user.address.city}${user.address.suite}${user.address.street}`
-        }))
-      })  
-  }
+        }));
+        setUserProfiles(data);
+      })
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
   return (
     <div className="App">
-      <button　onClick={onClickFetchUser}>データ取得</button>
-      <UserCard user={user}/>
+      <button onClick={onClickFetchUser}>データ取得</button>
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>データの取得に失敗しました</p>
+      ) : loading ? (
+        <p>Loading..</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
     </div>
   );
 }
