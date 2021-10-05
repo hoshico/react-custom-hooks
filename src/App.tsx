93b296ff1e65f487d2cfb655923c18915e7dadf9
hoshico <77495217+hoshico@users.noreply.github.com>
2021-10-05 13:47:40 +0900

    add fetch

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import "./styles.css";
 import { UserCard } from "./componets/UserCard"
 import axios from "axios";
 import { User } from "./types/api/user";
+import { userProfile } from "./types/userProfile";
+import { useState } from "react";
 
 const user = {
   id: "1",
@@ -11,8 +13,20 @@ const user = {
 }
 
 export default function App() {
-  const onClickFetchUser = () => {
-    axios.get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+const [useeProfiles, setUserProfiles] = useState<Array<userProfile>>([]);
+  
+const onClickFetchUser = () => {
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) =>({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`
+        }
+        ))
+      })  
   }
   return (
     <div className="App">
